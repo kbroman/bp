d45e748bd10aedde5f6d6fc27d79b8d082cfe4ec
Karl Broman <kbroman@gmail.com>
2014-06-03 14:15:04 -0500

    Roxygen-based help info

--- R/bp.R
+++ R/bp.R
@@ -1,3 +1,28 @@
+#' MCMC with bipartite graph 
+#'
+#' MCMC with bipartite graph
+#' @param out Root name for output files
+#' @param whole Input file with whole node info
+#' @param part Input file with part node info
+#' @param edge Input file with edge info
+#' @param alpha Parameter alpha (0 < alpha < beta < 1)
+#' @param beta Parameter alpha (0 < alpha < beta < 1)
+#' @param pi Parameter pi (0 < pi < 1)
+#' @param nburn Number of burn-in generations
+#' @param ngen Number of sample generations
+#' @param sub Subsample rate for burn-in and sample files
+#' @param penalty Penalty per illegal node to loglikelihood
+#' @param initial Initial state (see Details)
+#'
+#' @details The \code{initial} argument can take one of three values:
+#' \code{"inactive"} - all whole nodes inactive; \code{"random"} - all
+#' whole nodes active with probability \code{pi}, no illegal nodes; or
+#' \code{"high"} - all nodes with proportion of connected part nodes
+#' with response equal to 1 above 0.4 are active, no illegal nodes.
+#'
+#' @export
+#' @useDynLib bp
+#' @keywords models
 bp <-
 function(out="run1",  whole="T2D.whole", part="T2D.part", edge="T2D.edge",
          alpha=0.05, beta=0.2, pi=0.01,
