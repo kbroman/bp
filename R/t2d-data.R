02eb43262d4713856c1e0527e04e528ce4d2b5b6
Karl Broman <kbroman@gmail.com>
2014-06-04 11:04:59 -0500

    Add example data set

---
+++ R/t2d-data.R
@@ -0,0 +1,22 @@
+#' @name t2d
+#' @title Type 2 diabetes data
+#'
+#' @description Not entirely sure what these data are...
+#'
+#' @docType data
+#'
+#' @usage data(t2d)
+#'
+#' @format A list with three components:
+#' \code{whole}: list of character strings with the names of the "whole nodes";
+#' \code{part}: a vector of 0's and 1's; names are the names of the "part nodes";
+#' \code{edges}: a matrix of with two columns of character strings, with each
+#' row defining an edge between a "whole node" and a "part node".
+#'
+#' @keywords datasets
+#'
+#' @source Bret Larget
+#'
+#' @examples
+#' data(t2d)
+NULL
